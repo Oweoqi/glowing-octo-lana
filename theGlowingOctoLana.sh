572112012ffc2e40ab7b1ddd141eb4fe699dbe67
Anthony Cozamanis <anthony.cozamanis@chronicle.jp.net>
2015-03-04 10:08:33 +0800

    nmap pipe and input validation

--- theGlowingOctoLana.sh
+++ theGlowingOctoLana.sh
@@ -3,10 +3,23 @@
 ipr=$1
 cidr=$2
 
+if [ "$ipr" == "" ] || [ "$cidr" == "" ]
+then
+echo "No SUBNET or CIDR was entered, please re-run with: ./theGlowingOctoLana.sh <SUBNET> <CIDR>"
+exit
+fi
+
+# Check to ensure the script is run as root/sudo
+if [ "$(id -u)" != "0" ]; 
+then
+echo "This script must be run as root. Later hater." 1>&2
+exit 1
+fi
+
 mkdir $ipr
 cd $ipr
 
-sudo nmap -sP $ipr/$cidr -oG $ipr.txt
+nmap -sP $ipr/$cidr -oG $ipr.txt
 
 echo "Cleaning up the list...\n"
 
@@ -20,5 +33,5 @@ while read host;do grep $host $ipr.txt;done < $ipr\_cleaned.txt >> $host.txt
 
 echo "Now we enum...\n"
 
-while read host; do snmpcheck -t $host >> $host.txt; enum4linux $host >> $host.txt;done < $ipr\_cleaned.txt
+while read host;do nmap -sV $host >> $host.txt; snmpcheck -t $host >> $host.txt; enum4linux $host >> $host.txt;done < $ipr\_cleaned.txt
 
