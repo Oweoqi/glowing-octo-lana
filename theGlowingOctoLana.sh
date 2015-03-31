8a5c8ba9328eb8749463385ab192072980e20d4a
Anthony Cozamanis <anthony.cozamanis@chronicle.jp.net>
2015-03-31 09:33:29 +0800

    Removed background

--- theGlowingOctoLana.sh
+++ theGlowingOctoLana.sh
@@ -24,19 +24,43 @@ nmap -sP $ipr/$cidr -oG $ipr.txt
 echo -e "Cleaning up the list...\n"
 
 cat ./$ipr.txt | cut -d " " -f2 | sed '/Nmap/d' > $ipr\_cleaned.txt
+mv $ipr\_cleaned.txt $ipr.txt
 
 echo -e "Creating a mess of files...\n"
 
-while read host;do touch $host.txt;done < $ipr\_cleaned.txt
+while read host;do touch $host.txt;done < $ipr.txt
 
-while read host;do grep $host $ipr.txt;done < $ipr\_cleaned.txt >> $host.txt
+while read host;do grep $host $ipr.txt;done < $ipr.txt >> $host.txt
 
 echo -e "Now we enum...\n"
 
 while read host;do
 	echo -e "Scanning $host\n"
 	nmap -sS -sC -sV -O --min-rate=400 --min-parallelism=512 -p1-65535 -n -Pn -PS --open $host >> $host.txt 
-	snmpcheck -t $host >> $host.txt &
-	enum4linux $host >> $host.txt &
-done < $ipr\_cleaned.txt
+	snmpcheck-nothink -t $host >> $host.txt
+	enum4linux $host >> $host.txt
+done < $ipr.txt
 
+cd ..
+chown -hR $whoami:$whoami /u $ipr
+cd $ipr
+
+echo -e "Making a list of HTTP servers for the screenshoting...\n"
+
+grep "80/tcp\|8000/tcp\|443/tcp\|8443/tcp\|8080/tcp" *.txt > webservers.txt && cat webservers.txt | cut -d "/" -f1 | sed 's/.txt//' > eyewitness.lst
+
+EyeWitness.py --no-dns -f eyewitness.lst -d /tmp/$ipr\_webservers
+
+cp /tmp/$ipr\_webservers ./$ipr\_webservers
+
+chown -hR $whoami:$whoami /u ./$ipr\_webservers
+
+# Dumping ground for random ideas.
+# ================================
+# 
+# 
+# grep "25/tcp" *.txt > smtp_servers.txt
+# 
+# echo "slmail" | { read test; searchsploit $test; }
+# 
+# 
